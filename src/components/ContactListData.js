9772e456da9ad04ea4ac7338d2d87e0e9ef2caee
ocultofille <mansi.s.20015@gmail.com>
2024-04-12 17:25:51 +0530

    updating dark mode

---
+++ src/components/ContactListData.js
@@ -0,0 +1,16 @@
+// const ContactListData = ({title,description,hyperLinkText})=>{
+//     return(
+
+//         <div className=" border-bottom mb-4 p-1">
+                                    
+//         <div className="kanit-bold  mt-1 fw-normal  mb-3" style={{ color: "#3e4152", fontSize: "1.1rem" }} > {title}</div>
+//         <div>
+//             <div className="text-secondary " style={{ color: "rgba(40,44,63,0.45)", fontSize: "0.8rem" }} >{description}</div>
+//             <a className=" fw-bold " style={{textDecoration:"none",color:"#fc8019"}} href="#">{hyperLinkText}</a>
+//             <div className="text-uppercase mt-4  fw-bold " >{ options[0]?.type ? <span className=" p-2 " style={{color:"#fc8019", border:"1px solid #fc8019"}}> {"Send An "+ options[0]?.type} </span>: ""}</div>
+//             <div className="ms-3   text-secondary mt-2" style={{fontSize:"8px"}}>{options[0]?.waitTime}</div>
+//         </div>
+//     </div>
+//     )
+// }
+// export default ContactListData
